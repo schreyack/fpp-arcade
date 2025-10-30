4e84f1b3fac8195f91c3c2c6bcbbc480c31bc697
Tim Schreyack <schreyack@yahoo.com>
2025-10-30 06:51:04 -0700

    test

--- src/FPPPacman.cpp
+++ src/FPPPacman.cpp
@@ -113,12 +113,6 @@ public:
                 }
             }
         }
-        
-        // Print roomStartX and roomStartY in the center of the room
-        int centerX = startX + width / 2;
-        int centerY = startY + height / 2;
-        std::string coordStr = std::to_string(startX) + "," + std::to_string(startY);
-        outputString(coordStr, centerX - coordStr.length()/2, centerY);
     }
 
     PacmanEffect(PixelOverlayModel *m) : FPPArcadeGameEffect(m) {
@@ -159,9 +153,9 @@ public:
         int margin = 16; // Distance from edge to allow Pacman to fit through
         
         
-        // Top-left room
-        int roomStartX = margin;
-        int roomStartY = margin;
+        // Top-left room - centered in top-left quadrant
+        int roomStartX = cols/4 - roomWidth/2;
+        int roomStartY = rows/4 - roomHeight/2;
 
         drawRoom(roomStartX, roomStartY, roomWidth, roomHeight, rand() % 4);
         
